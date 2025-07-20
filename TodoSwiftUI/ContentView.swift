a442de793e8b6ed5052e8c0c4bfb79209f57769a
김종성 <jseongee@gmail.com>
2025-07-20 21:39:53 +0900

    feat: Add basic todo list

--- TodoSwiftUI/ContentView.swift
+++ TodoSwiftUI/ContentView.swift
@@ -1,14 +1,39 @@
 import SwiftUI
 
+struct TodoItem: Identifiable {
+    let id = UUID()
+    let content: String
+}
+
 struct ContentView: View {
+    @State var todos: [TodoItem] = []
+    @State var newTodo: String = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                TextField("할 일을 입력해주세요.", text: $newTodo)
+                    .textFieldStyle(.roundedBorder)
+                Button("추가") {
+                    addButtonTapped()
+                }
+                .buttonStyle(.borderedProminent)
+            }
+
+            List(todos) { todo in
+                Text(todo.content)
+            }
+
+            Spacer()
         }
         .padding()
+    }
+
+    private func addButtonTapped() {
+        if newTodo.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty { return }
+
+        todos.append(TodoItem(content: newTodo))
+        newTodo = ""
     }
 }
 
