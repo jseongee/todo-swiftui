fc4a18e0527e9a7e512dd66681bc3023cb23e9a1
김종성 <jseongee@gmail.com>
2025-07-20 22:44:34 +0900

    feat: Persist todo list as UserDefaults

--- TodoSwiftUI/ContentView.swift
+++ TodoSwiftUI/ContentView.swift
@@ -1,18 +1,20 @@
 import SwiftUI
 
-struct TodoItem: Identifiable {
-    let id = UUID()
+struct TodoItem: Identifiable, Codable {
+    let id: UUID
     let content: String
-    var isDone = false
+    var isDone: Bool
+
+    init(id: UUID = UUID(), content: String, isDone: Bool = false) {
+        self.id = id
+        self.content = content
+        self.isDone = isDone
+    }
 }
 
 struct ContentView: View {
-    @State var todos: [TodoItem] = [
-        TodoItem(content: "밥 먹기", isDone: true),
-        TodoItem(content: "산책하기"),
-        TodoItem(content: "커피 사기"),
-    ]
-    @State var newTodo: String = ""
+    @State private var todos: [TodoItem] = UserDefaults.standard.loadTodos()
+    @State private var newTodo: String = ""
 
     var body: some View {
         VStack {
@@ -32,6 +34,9 @@ struct ContentView: View {
                             .strikethrough(todo.isDone)
                             .foregroundColor(todo.isDone ? .gray : .primary)
                     }
+                    .onChange(of: todo.isDone) {
+                        saveTodos()
+                    }
 
                 }
                 .onDelete(perform: deleteTodo)
@@ -48,10 +53,35 @@ struct ContentView: View {
 
         todos.append(TodoItem(content: trimmed))
         newTodo = ""
+        saveTodos()
     }
 
     private func deleteTodo(at offsets: IndexSet) {
         todos.remove(atOffsets: offsets)
+        saveTodos()
+    }
+
+    private func saveTodos() {
+        UserDefaults.standard.saveTodos(todos)
+    }
+}
+
+extension UserDefaults {
+	private static let todosKey = "todos"
+
+    func loadTodos() -> [TodoItem] {
+        if let data = data(forKey: Self.todosKey),
+              let decoded = try? JSONDecoder().decode([TodoItem].self, from: data) {
+            return decoded
+        }
+
+        return []
+    }
+
+    func saveTodos(_ todos: [TodoItem]) {
+        if let encoded = try? JSONEncoder().encode(todos) {
+            set(encoded, forKey: Self.todosKey)
+        }
     }
 }
 
